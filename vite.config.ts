42f833667e8fa9707194d0aa1be1dc701351895e
AccessiT3ch <accessiT3ch@gmail.com>
2024-11-25 13:00:25 -0800

    Automate url base for production vs development

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,27 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        quietDeps: true,
-        additionalData: `@import "./src/scss/variables.scss";`,
+export default defineConfig(() => {
+  const env = process.env;
+  return {
+    plugins: [react()],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          quietDeps: true,
+          additionalData: `@import "./src/scss/variables.scss";`,
+        },
       },
     },
-  },
-  build: {
-    outDir: "docs",
-    emptyOutDir: true,
-  },
-  base: "/notsorry/",
+    build: {
+      outDir: "docs",
+      emptyOutDir: true,
+    },
+    define: {
+      'process.env': env,
+    },
+    // base url should be /notsorry/ for production and / for development
+    base: env.VITE_APP_ENV === "production" ? "/notsorry/" : "/",
+  };
 });
